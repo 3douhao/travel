f1b7d748e0d660de437aee2bf8fa20eeaf508403
3douhao <3douhao@gmail.com>
2019-01-27 17:35:38 +0800

    20190127-2

--- gulpfile.js
+++ gulpfile.js
@@ -9,6 +9,22 @@ const mixins = require('postcss-mixins')
 const nested = require('postcss-nested')
 const cssvars = require('postcss-simple-vars')
 const browserSync = require('browser-sync').create()
+const svgSprite = require('gulp-svg-sprite')
+const rename = require('gulp-rename')
+const del = require('del')
+const config = {
+  mode: {
+    css: {
+      sprite: 'sprite.svg',
+      render: {
+        css: {
+          template: './gulp/templates/sprite.css'
+        }
+      }
+
+    }
+  }
+}
 const styles = function (done) {
   return src('./app/assets/styles/styles.css').pipe(postcss([cssImport, mixins, cssvars, nested, autoprefixer])).pipe(dest('./app/temp/styles'))
   done()
@@ -37,4 +53,29 @@ watch('./app/index.html', function (done) {
   done()
 })
 watch('./app/assets/styles/**/*.css', series(styles, cssInject))
-exports.bs = bs
+
+const svgCreate = function (done) {
+  return src('./app/assets/images/icons/**/*.svg')
+    .pipe(svgSprite(config))
+    .pipe(dest('./app/temp/sprite/'))
+}
+
+const copySpriteCss = function (done) {
+  return src('./app/temp/sprite/css/*.css')
+    .pipe(rename('_sprite.css'))
+    .pipe(dest('./app/assets/styles/modules/'))
+}
+const copySpriteImage = function (done) {
+  return src('./app/temp/sprite/css/**/*.svg')
+    .pipe(dest('./app/assets/images/sprites'))
+}
+
+const clean = function (done) {
+  return del('./app/temp/sprite', './app/assets/images/sprites')
+}
+
+exports.icons = series(svgCreate, copySpriteCss, copySpriteImage)
+
+exports.bs = series(clean, svgCreate, copySpriteCss, copySpriteImage, bs)
+exports.svg = svgCreate
+exports.cp = copySpriteCss
